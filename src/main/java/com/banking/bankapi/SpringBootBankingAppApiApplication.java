2653ae4306c55687690e3b0dae8b058d289cd66e
KA-devl <anes1999@live.ca>
2023-05-28 04:15:57 -0700

    CreatedAt and updatedAt with jpa auditing

--- src/main/java/com/banking/bankapi/SpringBootBankingAppApiApplication.java
+++ src/main/java/com/banking/bankapi/SpringBootBankingAppApiApplication.java
@@ -2,8 +2,12 @@ package com.banking.bankapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.persistence.EntityListeners;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringBootBankingAppApiApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/banking/bankapi/models/AbstractEntity.java
+++ src/main/java/com/banking/bankapi/models/AbstractEntity.java
@@ -3,23 +3,33 @@ package com.banking.bankapi.models;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @SuperBuilder
 @NoArgsConstructor
 @MappedSuperclass //Information is inherited by other classes, there is no table for this class, it is used to avoid code duplication
+@EntityListeners(AuditingEntityListener.class) //Used to automatically update the creationDate and lastModifiedDate. Must also be added to the main class
 public class AbstractEntity {
 
     @Id
     @GeneratedValue
     private Integer id;
 
+    @CreatedDate
+    @Column(
+            name = "createdDate",
+            nullable = false,
+            updatable = false
+    )
     private LocalDateTime creationDate;
 
-    private LocalDateTime lastUpdated;
+    @LastModifiedDate
+    @Column(name = "lastModifiedDate")
+    private LocalDateTime lastModifiedDate;
 }
