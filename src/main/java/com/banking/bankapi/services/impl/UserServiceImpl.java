7c4279e9d42b541fa2f573366b79d673925558d1
KA-devl <anes1999@live.ca>
2023-05-31 10:18:25 -0700

    UserService implementation

--- src/main/java/com/banking/bankapi/services/impl/UserServiceImpl.java
+++ src/main/java/com/banking/bankapi/services/impl/UserServiceImpl.java
@@ -8,34 +8,41 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-
 
     private final UserRepository userRepository;
     private final ObjectsValidator<UserDto> validator;
 
     @Override
     public Integer save(UserDto dto) {
-        validator.validate(dto);
+        validator.validate(dto); //validate the object before saving it
         return userRepository.save(UserDto.toEntity(dto)).getId();
     }
 
     @Override
     public List<UserDto> findAll() {
-        return null;
+        return userRepository.findAll()
+                .stream()//Transform all users to userDto
+                .map(user -> UserDto.fromEntity(user))
+                .collect(Collectors.toList());
     }
 
     @Override
     public UserDto findById(Integer id) {
-        return null;
+        return userRepository.findById(id)
+                .map(user -> UserDto.fromEntity(user))
+                .orElseThrow(() -> new EntityNotFoundException("User not found with id: " + id));
     }
 
     @Override
     public void delete(Integer id) {
-
+        //check before delete
+        userRepository.deleteById(id);
     }
 }
