f5cd51d8e4be80f866276bd6fe4a2bd27abf9c7d
KA-devl <anes1999@live.ca>
2023-05-31 19:04:04 -0700

    Contact controller impl

---
+++ src/main/java/com/banking/bankapi/controllers/ContactController.java
@@ -0,0 +1,45 @@
+package com.banking.bankapi.controllers;
+
+import com.banking.bankapi.dto.ContactDto;
+import com.banking.bankapi.services.ContactService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contacts")
+@RequiredArgsConstructor
+public class ContactController {
+
+    private final ContactService service;
+
+    @PostMapping("/")
+    public ResponseEntity<Integer> save(
+            ContactDto contactDto
+    ) {
+        return ResponseEntity.ok(service.save(contactDto));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<ContactDto>> findAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{contact-id}")
+    public ResponseEntity<ContactDto> findById(
+          @PathVariable("contact-id") Integer contactId
+    ) {
+        return ResponseEntity.ok(service.findById(contactId));
+    }
+
+    @DeleteMapping("/{contact-id}")
+    public ResponseEntity<Void> delete(
+            @PathVariable("contact-id") Integer contactId
+    ) {
+        service.delete(contactId);
+        return ResponseEntity.accepted().build();
+    }
+
+}
