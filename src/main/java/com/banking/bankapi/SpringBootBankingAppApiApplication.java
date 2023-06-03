1994d6b0b83ab3c04c545485cee2b30163bc8713
KA-devl <anes1999@live.ca>
2023-06-02 19:59:02 -0700

    Adding user on launch

--- src/main/java/com/banking/bankapi/SpringBootBankingAppApiApplication.java
+++ src/main/java/com/banking/bankapi/SpringBootBankingAppApiApplication.java
@@ -1,7 +1,11 @@
 package com.banking.bankapi;
 
+import com.banking.bankapi.dto.UserDto;
+import com.banking.bankapi.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.persistence.EntityListeners;
@@ -14,4 +18,10 @@ public class SpringBootBankingAppApiApplication {
         SpringApplication.run(SpringBootBankingAppApiApplication.class, args);
     }
 
+    	@Bean     // CommandLineRunner is an interface used to indicate that a bean should run when it is contained within a SpringApplication. 
+        CommandLineRunner run(UserRepository userRepository) {
+            return args -> {
+			userRepository.save(UserDto.toEntity(UserDto.builder().email("johncena@gmail.com").firstname("John").lastname("Cena").password("123456").build()));
+		};
+	}
 }
